e91b644fe7bd26e0bfb19a1743ab6ff82511e847
Josh Solis <joshdsolis@gmail.com>
2019-07-23 17:25:19 -0500

    forbes scraper script

--- forbes_scraper.py
+++ forbes_scraper.py
@@ -6,44 +6,47 @@ import re
 
 search = 'facebook'
 
-source = urlopen('https://www.forbes.com/search/?q=' + search)
-soup = BeautifulSoup(source,'lxml')
+def forbes_scraper(search)
+    source = urlopen('https://www.forbes.com/search/?q=' + search)
+    soup = BeautifulSoup(source,'lxml')
 
-lst = []
-for i in soup.findAll('a', {'class': "stream-item__title"}):
-    title = i.text
-    link = i.get('href')
+    lst = []
+    for i in soup.findAll('a', {'class': "stream-item__title"}):
+        title = i.text
+        link = i.get('href')
 
-    title = title.replace('\t','')
-    title = title.replace('\n','')
-    title = title.strip()
+        title = title.replace('\t','')
+        title = title.replace('\n','')
+        title = title.strip()
 
-    lst.append({'title':title, 'link':link})
+        lst.append({'title':title, 'link':link})
 
-df = pd.DataFrame.from_dict(lst)
+    df = pd.DataFrame.from_dict(lst)
 
-text_list = []
-for link in df['link']:
-    source2 = urlopen(link)
-    soup2 = BeautifulSoup(source2, 'lxml')
-    text = soup2.findAll('p', text=re.compile("."))
-    text_list.append(text)
+    text_list = []
+    for link in df['link']:
+        source2 = urlopen(link)
+        soup2 = BeautifulSoup(source2, 'lxml')
+        text = soup2.findAll('p', text=re.compile("."))
+        text_list.append(text)
 
-def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    return cleantext
+    def cleanhtml(raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
 
-new_text_list = []
-for text in text_list:
-    new_text_list.append(cleanhtml(str(text)))
-    
-final_text_list = []
-for text in new_text_list:
-    text = text.replace("[", "")
-    text = text.replace("]", "")
-    text = text.replace("\\", "")
-    text = text.replace("\xa0", " ")
-    final_text_list.append(text)
+    new_text_list = []
+    for text in text_list:
+        new_text_list.append(cleanhtml(str(text)))
+        
+    final_text_list = []
+    for text in new_text_list:
+        text = text.replace("[", "")
+        text = text.replace("]", "")
+        text = text.replace("\\", "")
+        text = text.replace("\xa0", " ")
+        final_text_list.append(text)
+    return final_text_list
 
-print(final_text_list)
+
+forbes_scraper(search)
